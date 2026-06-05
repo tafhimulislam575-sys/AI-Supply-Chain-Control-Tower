ea26d95ef6911eab86fce5692306918460502e24
Tafhimul Islam <tafhimulislam74@gmail.com>
2026-06-05 16:25:32 +0200

    Added supplier performance chart

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,43 @@
 async function loadData() {
-    const response = await fetch("http://127.0.0.1:5000/dashboard");
-    const data = await response.json();
 
-    document.getElementById("suppliers").innerText = data.suppliers;
-    document.getElementById("orders").innerText = data.orders;
-    document.getElementById("delivery").innerText = data.delivery;
+    const response =
+        await fetch("http://127.0.0.1:5000/dashboard");
+
+    const data =
+        await response.json();
+
+    document.getElementById("suppliers").innerText =
+        data.suppliers;
+
+    document.getElementById("orders").innerText =
+        data.orders;
+
+    document.getElementById("delivery").innerText =
+        data.delivery + "%";
+
+    const labels =
+        data.risk_analysis.map(
+            x => x.supplier
+        );
+
+    const scores =
+        data.risk_analysis.map(
+            x => x.score
+        );
+
+    new Chart(
+        document.getElementById("riskChart"),
+        {
+            type: "bar",
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: "Supplier Score",
+                    data: scores
+                }]
+            }
+        }
+    );
 }
 
 loadData();
